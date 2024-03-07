c0a32c97ec8c5e3c33c1eb83c2dbafc68861b947
Ijeoma <adatimo01@gmail.com>
2024-03-07 16:00:03 +0100

    changes to the httpClient

--- client/src/Services/httpClient.ts
+++ client/src/Services/httpClient.ts
@@ -3,7 +3,7 @@ import { handleRefreshToken } from "./authServices";
 
 // Create a new Axios instance
 const axiosInstance = axios.create({
-  baseURL: "https://shorten-link-api.vercel.app/",
+  baseURL: "https://shorten-link-api.vercel.app",
 });
 
 // Add a request interceptor
