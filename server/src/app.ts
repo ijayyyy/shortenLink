66a721e5a465b129022ee71a2b2d6405781d1789
Ijeoma <adatimo01@gmail.com>
2024-03-12 14:43:16 +0100

    changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,12 +6,12 @@ import dbConnect from "./config/db";
 import baseRouter from "./routes";
 
 const app = express();
-
+ 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
    credentials: true,
-    origin: ["http://localhost:3000"], 
+    origin: ["https://scissor-app-jdpp.onrender.com/"], 
    methods: ["POST", "GET"] 
   }));
 
