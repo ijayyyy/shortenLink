982ef2edd88a9af3a81aca8958d0c19e72034bff
Ijeoma <adatimo01@gmail.com>
2024-03-15 20:19:10 +0100

    chnage

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
    credentials: true,
-    origin: ["*"], 
+    origin: ["https://delicate-cocada-132c97.netlify.app"], 
    methods: ["POST", "GET"] 
   }));
 
